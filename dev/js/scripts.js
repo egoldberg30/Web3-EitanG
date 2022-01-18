5a97267877ee9a9be2b1125485ca19b3ff53f719
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-01-18 11:37:18 -0500

    getbigger

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,6 @@
 // import { skillsTrigger } from "./scrollAnimation.js"
 
-import { slideInRight, slideInLeft} from "./scrollAnimation.js"
+import { slideInRight, slideInLeft, getbigger} from "./scrollAnimation.js"
 
 
 window.addEventListener('load', function(){
@@ -8,7 +8,10 @@ window.addEventListener('load', function(){
     let triggerElements = ["#history-middle","#history-first", "#skills-content"];
 
     let animationElements =["#history-middle p","#robot-profile", "#skills-content h1"];
+  
+    let growingElements =["#history-first h1"];
         
 {slideInLeft(triggerElements[i], animationElements[i]);}
 {slideInRight(triggerElements[i], animationElements[i]);}
+{getbigger(triggerElements[i], animationElements[i]);}
 });
--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -4,43 +4,44 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 
-export function slideInRight(triggerElement,animationElement){
+// export function slideInRight(triggerElement,animationElement){
+//     const tl = new gsap.timeline();
+//     tl.from(animationElement,{
+//         duration:1,
+//         x: -500,
+//         alpha:0,
+//         scrollTrigger:{
+//             trigger: triggerElement,
+//             scrub:true,
+//             toggleActions: "restart none resume none",
+//             markers:true
+//         }
+//     });
+// }
+
+
+// export function slideInLeft(triggerElement,animationElement){
+//     const tl = new gsap.timeline();
+//     tl.from(animationElement,{
+//         duration:1,
+//         x: 500,
+//         alpha:0,
+//         scrollTrigger:{
+//             trigger: triggerElement,
+//             scrub:true,
+//             toggleActions: "restart none resume none",
+//             markers:true
+//         }
+//     });
+// }
+
+export function getbigger(triggerElement,animationElement){
     const tl = new gsap.timeline();
     tl.from(animationElement,{
         duration:1,
-        x: -500,
         alpha:0,
         scrollTrigger:{
             trigger: triggerElement,
-            scrub:true,
-            toggleActions: "restart none resume none",
-            markers:true
-        }
-    });
-}
-
-
-export function slideInLeft(triggerElement,animationElement){
-    const tl = new gsap.timeline();
-    tl.from(animationElement,{
-        duration:1,
-        x: 500,
-        alpha:0,
-        scrollTrigger:{
-            trigger: triggerElement,
-            scrub:true,
-            toggleActions: "restart none resume none",
-            markers:true
-        }
-    });
-}
-
-export function grow(growingElements){
-    const tl = new gsap.timeline();
-    tl.from(animationElement,{
-        duration:1,
-        alpha:0,
-        scrollTrigger:{
             toggleActions: "play none none none",
             scrub:true,
             markers:true
