a8ad0282686cf8d386bd3d3901fb4a308283f236
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-01-18 10:50:27 -0500

    clean up

--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -34,37 +34,3 @@ export function slideInLeft(triggerElement,animationElement ){
         }
     });
 }
-
-
-// gsap.set(".gallery",{transformOrigin:"left center"});
-
-
-// export function skillsTrigger(originDirection, animationElement, triggerElement){
-//     const tl = new gsap.timeline();
-
-//     tl.from(animationElement,{
-//         // duration:3, 
-//         scaleX:0,
-//         transformOrigin: originDirection,
-//         scrollTrigger: {
-//             trigger: animationElement,
-//             start:"top 60%",
-//             end: "bottom 80%",
-//             toggleActions: "restart none resume none",
-//             scrub: 2,
-//             pin: "#skills",
-//             markers: true
-//         },
-//         stagger:0.25
-//     });
-// }
-
-
-
-
-
-
-// export function demoThing(){
-    // console.log("this is working here too!");
-    // console.log("working");
-// }
