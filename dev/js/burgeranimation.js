14cb11f1d4282bbf832567095d262fa96fd646fa
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-01-27 12:56:06 -0500

    switche rotation

--- dev/js/burgeranimation.js
+++ dev/js/burgeranimation.js
@@ -4,11 +4,11 @@ gsap.set(".burgerlines",{transformOrigin:"center"});
 
 const toplineTL = new gsap.timeline();
 toplineTL.to(".burgerlines:nth-child(1)",{duration:0.2, y:"+=8"})
-.to(".burgerlines:nth-child(1)",{duration:0.2, rotation:-45,fill:"#fff"});
+.to(".burgerlines:nth-child(1)",{duration:0.2, rotation:45,fill:"#fff"});
 
 const bottomlineTL = new gsap.timeline();
 bottomlineTL.to(".burgerlines:nth-child(3)",{duration:0.2, y:"-=8"})
-.to(".burgerlines:nth-child(3)",{duration:0.2, rotation:45,fill:"#fff"});
+.to(".burgerlines:nth-child(3)",{duration:0.2, rotation:-45,fill:"#fff"});
 
 const middlelineTL = new gsap.timeline();
 middlelineTL.to(".burgerlines:nth-child(2)",{duration:0.2, scale:0})
