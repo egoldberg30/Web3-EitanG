bcfeffb42c6f35b33d348717b926b1d17df3a2ef
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-01-27 10:02:01 -0500

    clean up

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function(){
 
     let animationElements = ["#history-first h4", "#history-last"];
     let triggerElements = ["#history-first h4", "#history-last"];
-    let growingElements =["#history-first h1"];
+    let growingElements = ["#history-first h1"];
 
 {slideInLeft(triggerElements[0], animationElements[0]);}
 {slideInLeft(triggerElements[1], animationElements[1]);}
