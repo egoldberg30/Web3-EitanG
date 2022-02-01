49e6209f19aba5ff4b9707ebee53a83c52b9698a
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-02-01 10:01:00 -0500

    adjusted burger animation

--- dev/js/burgeranimation.js
+++ dev/js/burgeranimation.js
@@ -3,12 +3,12 @@ import { gsap } from "gsap";
 gsap.set(".burgerlines",{transformOrigin:"center"});
 
 const toplineTL = new gsap.timeline();
-toplineTL.to(".burgerlines:nth-child(1)",{duration:0.2, y:"+=8"})
-.to(".burgerlines:nth-child(1)",{duration:0.2, rotation:45,fill:"#fff"});
+toplineTL.to(".burgerlines:nth-child(1)",{duration:0.2, y:"-=8"})
+.to(".burgerlines:nth-child(1)",{duration:0.2, rotation:-45,fill:"#fff"});
 
 const bottomlineTL = new gsap.timeline();
-bottomlineTL.to(".burgerlines:nth-child(3)",{duration:0.2, y:"-=8"})
-.to(".burgerlines:nth-child(3)",{duration:0.2, rotation:-45,fill:"#fff"});
+bottomlineTL.to(".burgerlines:nth-child(3)",{duration:0.2, y:"+=8"})
+.to(".burgerlines:nth-child(3)",{duration:0.2, rotation:45,fill:"#fff"});
 
 const middlelineTL = new gsap.timeline();
 middlelineTL.to(".burgerlines:nth-child(2)",{duration:0.2, scale:0})
