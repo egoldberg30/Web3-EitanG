89718b466a3b3e9e9cdbf84d468bf8c6ea44abd7
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-02-10 09:36:41 -0500

    menu functionality

--- dev/js/mobileResizing.js
+++ dev/js/mobileResizing.js
@@ -7,13 +7,18 @@
 //     // let menuWidth = menu.offsetWidth;
 
     
-//    // check the view port view and see if the menu needs to be moved
-//    if(document.documentElement.clientWidth <= 1024){
-//     // console.log("hide");
-//     gsap.set("#nav-container",{right: 0});
-//     }else{
-//         // console.log("un-hide");
-//         gsap.set("#nav-container",{x:0});
-//     }
+// //    // check the view port view and see if the menu needs to be moved
+// //    if(document.documentElement.clientWidth <= 1024){
+// //     // console.log("hide");
+// //     gsap.set("#nav-container",{right: 0});
+// //     }
+// //     else{
+// //         // console.log("un-hide");
+// //         gsap.set("#nav-container",{alpha:1});
+// //     }
 
+
+//     if(document.documentElement.clientWidth > 1024){
+//         gsap.set("#nav-container",{alpha:1});
+//     }
 // }
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,8 @@
 import {burgerTL} from "./burgerAnimation";
 import {menuAnimation} from "./mobileMenu";
+import { gsap } from "gsap";
 // import {displayWindowSize} from "./mobileResizing.js"
-import {scrollPage} from "./pageScroll"
+// import {scrollPage} from "./pageScroll"
 
 var burgerButton = document.querySelector("#hamburger_icon");
 let canISeeMenu = false;
@@ -24,27 +25,56 @@ burgerButton.addEventListener("click", openCloseMenu);
 let navButtons = document.querySelectorAll(".nav-links");
 
 for (const button of navButtons){
-    button.addEventListener("click", checkScrolling);
+    // button.addEventListener("click", checkScrolling);
     button.addEventListener("click", openCloseMenu);
+    // button.addEventListener("click", textHighlight);
 }
 
 
-
-
-
-function checkScrolling(e) {
-    // check to see which button was clicked
-    const indexValue = [].indexOf.call(navButtons, e.target)
-    if (indexValue != -1) {
-        scrollPage(indexValue);
+function displayNav(){
+    if(document.documentElement.clientWidth > 1024){
+        gsap.set("#nav-container",{alpha:1});
+    }
+    else{
+        gsap.set("#nav-container",{alpha:0});
     }
 }
 
+window.addEventListener("resize", displayNav);
+window.addEventListener("load", displayNav);
 
 
 
-//listen to window resize
+
+
+// function checkScrolling(e) {
+//     // check to see which button was clicked
+//     const indexValue = [].indexOf.call(navButtons, e.target)
+//     if (indexValue != -1) {
+//         scrollPage(indexValue);
+//     }
+// }
+
+
+
+
+// // listen to window resize
 // window.addEventListener("resize", displayWindowSize);
 
-//check the window size on load
-// window.addEventListener('load', displayWindowSize); 
+// // check the window size on load
+// window.addEventListener('load', displayWindowSize);
+
+
+
+// let canIseeRed = false;
+
+// function textHighlight(){
+//     if(canIseeRed === false){
+//         gsap.to ("#redlink", {color:black});
+//         // gsap.set("#nav-container",{x:0});
+//         canIseeRed = true;
+//     }else{
+//         gsap.to ("#redlink", {color:$red});
+//         canIseeRed = false;
+//     }
+// }
