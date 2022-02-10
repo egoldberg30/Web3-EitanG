ba9971d3b0b9abcda5ffd3e22bb00e7e3db28669
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2022-02-10 09:09:41 -0500

    menu opacity

--- dev/js/mobileMenu.js
+++ dev/js/mobileMenu.js
@@ -1,4 +1,4 @@
 import {gsap} from "gsap";
 
 export const menuAnimation = new gsap.timeline({paused:true});
-menuAnimation.from("#nav-container",{duration:0.5, y:0, x:0});
+menuAnimation.to("#nav-container",{alpha:1, duration:0.5});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,7 @@
 import {burgerTL} from "./burgerAnimation";
 import {menuAnimation} from "./mobileMenu";
+// import {displayWindowSize} from "./mobileResizing.js"
+import {scrollPage} from "./pageScroll"
 
 var burgerButton = document.querySelector("#hamburger_icon");
 let canISeeMenu = false;
@@ -22,6 +24,27 @@ burgerButton.addEventListener("click", openCloseMenu);
 let navButtons = document.querySelectorAll(".nav-links");
 
 for (const button of navButtons){
-    // button.addEventListener("click", checkScrolling);
+    button.addEventListener("click", checkScrolling);
     button.addEventListener("click", openCloseMenu);
 }
+
+
+
+
+
+function checkScrolling(e) {
+    // check to see which button was clicked
+    const indexValue = [].indexOf.call(navButtons, e.target)
+    if (indexValue != -1) {
+        scrollPage(indexValue);
+    }
+}
+
+
+
+
+//listen to window resize
+// window.addEventListener("resize", displayWindowSize);
+
+//check the window size on load
+// window.addEventListener('load', displayWindowSize); 
